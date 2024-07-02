230352d261bafb78d6c9e845b414ab2366889ef4
yul <yulmii0914@gmail.com>
2024-07-02 16:24:02 +0900

    더미데이터 바꿈

--- src/main/java/com/harmony/gw/HarmonyApplication.java
+++ src/main/java/com/harmony/gw/HarmonyApplication.java
@@ -36,16 +36,16 @@ public class HarmonyApplication implements CommandLineRunner {
 		urepository.save(new Employee("admin", "$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW",  "황인환", "A", "010-0000-0000", "usejin00@0mail.com", "대표이사" ,"USER", LocalDate.of(2024,05,30)));
 	
 		 Schedule[] schedules = {
-		            new Schedule(null, "Title 1", LocalDateTime.of(LocalDate.of(2023, 7, 1), LocalTime.of(10, 0)), LocalDateTime.of(LocalDate.of(2023, 7, 1), LocalTime.of(17, 0)), false),
-		            new Schedule(null, "Title 2", LocalDateTime.of(LocalDate.of(2023, 7, 2), LocalTime.of(10, 0)), LocalDateTime.of(LocalDate.of(2023, 7, 2), LocalTime.of(18, 0)), true),
-		            new Schedule(null, "Title 3", LocalDateTime.of(LocalDate.of(2023, 7, 3), LocalTime.of(8, 0)), LocalDateTime.of(LocalDate.of(2023, 7, 3), LocalTime.of(16, 0)), true),
-		            new Schedule(null, "Title 4", LocalDateTime.of(LocalDate.of(2023, 7, 4), LocalTime.of(9, 30)), LocalDateTime.of(LocalDate.of(2023, 7, 4), LocalTime.of(17, 30)), true),
-		            new Schedule(null, "Title 5", LocalDateTime.of(LocalDate.of(2023, 7, 5), LocalTime.of(9, 0)), LocalDateTime.of(LocalDate.of(2023, 7, 5), LocalTime.of(17, 0)), true),
-		            new Schedule(null, "Title 6", LocalDateTime.of(LocalDate.of(2023, 7, 6), LocalTime.of(10, 10)), LocalDateTime.of(LocalDate.of(2023, 7, 6), LocalTime.of(18, 0)), false),
-		            new Schedule(null, "Title 7", LocalDateTime.of(LocalDate.of(2023, 7, 7), LocalTime.of(8, 20)), LocalDateTime.of(LocalDate.of(2023, 7, 7), LocalTime.of(16, 0)), true),
-		            new Schedule(null, "Title 8", LocalDateTime.of(LocalDate.of(2023, 7, 8), LocalTime.of(9, 30)), LocalDateTime.of(LocalDate.of(2023, 7, 8), LocalTime.of(17, 30)), false),
-		            new Schedule(null, "Title 9", LocalDateTime.of(LocalDate.of(2023, 7, 9), LocalTime.of(9, 0)), LocalDateTime.of(LocalDate.of(2023, 7, 9), LocalTime.of(17, 0)), false),
-		            new Schedule(null, "Title 10", LocalDateTime.of(LocalDate.of(2023, 7, 10), LocalTime.of(10, 25)), LocalDateTime.of(LocalDate.of(2023, 7, 10), LocalTime.of(18, 0)), true)
+		            new Schedule(null, "Title 1", LocalDateTime.of(LocalDate.of(2024, 7, 1), LocalTime.of(10, 0)), LocalDateTime.of(LocalDate.of(2024, 7, 1), LocalTime.of(17, 0)), false),
+		            new Schedule(null, "Title 2", LocalDateTime.of(LocalDate.of(2024, 7, 2), LocalTime.of(10, 0)), LocalDateTime.of(LocalDate.of(2024, 7, 2), LocalTime.of(18, 0)), true),
+		            new Schedule(null, "Title 3", LocalDateTime.of(LocalDate.of(2024, 7, 3), LocalTime.of(8, 0)), LocalDateTime.of(LocalDate.of(2024, 7, 3), LocalTime.of(16, 0)), true),
+		            new Schedule(null, "Title 4", LocalDateTime.of(LocalDate.of(2024, 7, 4), LocalTime.of(9, 30)), LocalDateTime.of(LocalDate.of(2024, 7, 4), LocalTime.of(17, 30)), true),
+		            new Schedule(null, "Title 5", LocalDateTime.of(LocalDate.of(2024, 7, 5), LocalTime.of(9, 0)), LocalDateTime.of(LocalDate.of(2024, 7, 10), LocalTime.of(17, 0)), true),
+		            new Schedule(null, "Title 6", LocalDateTime.of(LocalDate.of(2024, 7, 6), LocalTime.of(10, 10)), LocalDateTime.of(LocalDate.of(2024, 7, 6), LocalTime.of(18, 0)), false),
+		            new Schedule(null, "Title 7", LocalDateTime.of(LocalDate.of(2024, 7, 7), LocalTime.of(8, 20)), LocalDateTime.of(LocalDate.of(2024, 7, 7), LocalTime.of(16, 0)), true),
+		            new Schedule(null, "Title 8", LocalDateTime.of(LocalDate.of(2024, 7, 8), LocalTime.of(9, 30)), LocalDateTime.of(LocalDate.of(2024, 7, 8), LocalTime.of(17, 30)), false),
+		            new Schedule(null, "Title 9", LocalDateTime.of(LocalDate.of(2024, 7, 9), LocalTime.of(9, 0)), LocalDateTime.of(LocalDate.of(2024, 7, 9), LocalTime.of(17, 0)), false),
+		            new Schedule(null, "Title 10", LocalDateTime.of(LocalDate.of(2024, 7, 10), LocalTime.of(10, 25)), LocalDateTime.of(LocalDate.of(2024, 7, 10), LocalTime.of(18, 0)), true)
 		        };
 
 	        for (Schedule schedule : schedules) {
