55c9252484c87c81e1e8b89dc02ee407f6594368
yul <yulmii0914@gmail.com>
2024-07-05 19:57:07 +0900

    Reapply "로그인 가능 제한 입사후 퇴사전 가능"
    This reverts commit ef0bae49bff2c27a70c5d7f3aa1e11148093bcdd.

--- src/main/java/com/harmony/gw/repository/EmployeeRepository.java
+++ src/main/java/com/harmony/gw/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.harmony.gw.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.harmony.gw.entity.Employee;
@@ -10,5 +12,6 @@ import com.harmony.gw.entity.Employee;
 @RepositoryRestResource(exported = false)
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
-	Optional<Employee> findByEmpNo(String empNo);
+	 @Query("SELECT e FROM Employee e WHERE e.empNo = :empNo AND e.delYN = 'N' AND e.hireDate <= CURRENT_DATE AND (e.outDate IS NULL OR e.outDate >= CURRENT_DATE)")
+	    Optional<Employee> findByEmpNo(@Param("empNo") String empNo);
 }
